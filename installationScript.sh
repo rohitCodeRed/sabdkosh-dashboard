4031a2e0ee3eedb2b276297d727c22324d234b59
rohitCodeRed <rohit06051993@gmail.com>
2021-01-26 17:08:49 +0530

    Added a shell script for installing all npm packages related nodeserver and angular Cli directory

--- installationScript.sh
+++ installationScript.sh
@@ -1,1 +1,22 @@
-#TODO..
+#!/bin/shins    
+
+#Go to directory angularCli.
+cd ./angularCli
+
+# Run npm install for angular project.
+npm install
+
+#Return back to root of project
+cd ..
+
+#Go to directory nodeServer
+cd ./nodeServer
+
+#install all npm packages related nodeServer.
+npm install
+
+#return back to root.
+cd ..
+
+#exit
+exit
